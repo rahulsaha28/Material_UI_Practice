e613f5535115dfd85c15e6cf8deb4f5d0668bb04
Rahul <saharahul039@gmail.com>
2022-02-13 11:52:28 +0600

    class 2 is added.

---
+++ src/components/Home/HomeComponent.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const HomeComponent = () => {
+  return (
+    <div>
+        <Typography gutterBottom noWrap={true} variant="subtitle1" color="indigo">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempore laborum ea odit optio blanditiis totam! Fuga quod, hic cupiditate mollitia dignissimos delectus perspiciatis quibusdam ullam facilis est vel odit.</Typography>
+    </div>
+  )
+}
+
+export default HomeComponent
