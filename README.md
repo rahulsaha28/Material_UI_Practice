7bd378664d10388f1082ef1c6ab76820eed8b44f
Rahul <saharahul039@gmail.com>
2022-02-14 18:49:18 +0600

    added class 5

--- README.md
+++
@@ -1,3 +0,0 @@
-## This is the solution of icons-material 
-
->> yarn add @mui/material-icons --network-timeout 500000
--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -11,49 +11,39 @@ import {
 import React, { useState } from "react";
 import  { KeyboardArrowLeft, KeyboardArrowRight, UploadFile, PhotoCamera } from '@mui/icons-material'
 
+import { makeStyles } from '@mui/styles'
+
+
+const useStyles = makeStyles({
+  myClass:{
+    background:"linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    fontSize:'90px !important',
+    '& :hover':{
+      backgroundColor:'blue'
+    }
+  }
+})
+
 const HomeComponent = () => {
+  const classes  = useStyles();
+
+  console.log(classes)
+
   const [value, setValue] = useState(true);
+
+
+
   return (
     <div>
-      <Typography gutterBottom noWrap={true} variant="subtitle1" color="indigo">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempore
-        laborum ea odit optio blanditiis totam! Fuga quod, hic cupiditate
-        mollitia dignissimos delectus perspiciatis quibusdam ullam facilis est
-        vel odit.
-      </Typography>
-      <Button variant="contained" color="success">
+     
+      <Button className={classes.myClass} variant="contained">
         Next
       </Button>
-      <LoadingButton
-        loadingIndicator={<CircularProgress color="primary" size={30} />}
-        loading={value ? true : false}
-        onClick={() => console.log("hi")}
-      >
-        Loading
-      </LoadingButton>
-      <Container maxWidth="xs">
-        <ButtonGroup variant="contained" color="primary">
-          <Button onClick={() => console.log("left")}>
-            <KeyboardArrowLeft fontSize="large" color="secondary" />
-          </Button>
+      
           
-          <Button onClick={() => console.log("right")}>
-            <KeyboardArrowRight/>
-          </Button>
-          
-        </ButtonGroup>
         
-        <label>
-          <Input onChange={e=>{
-            const file = new FormData();
-            file.set('file', e.target.files[0]);
-            console.log(e.target.files[0])
-          }} style={{ display:"none" }} type="file" />
-          <IconButton component="span">
-            <PhotoCamera/>
-          </IconButton>
-        </label>
-      </Container>
+        
+        
     </div>
   );
 };
