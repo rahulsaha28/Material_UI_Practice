ab6049bb7a7a8957a1852333041514087f39b6c9
Rahul <saharahul039@gmail.com>
2022-02-17 20:27:11 +0600

    added all in class 13

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -1,7 +1,6 @@
 import { LoadingButton } from "@mui/lab";
 import {
-  Button, Select, Paper, Card, CardContent,
-   Typography, CardHeader, Avatar, IconButton, CardMedia
+  Button, Select, Paper, Card, CardContent, Drawer, Typography
 } from "@mui/material";
 import React, { useState } from "react";
 import  { KeyboardArrowLeft } from '@mui/icons-material';
@@ -13,37 +12,23 @@ import { useForm } from 'react-hook-form';
 
 const HomeComponent = () => {
 
- const { register, handleSubmit, watch, formState:{ errors } }  = useForm()
-
-  const handelForm = (e)=>{
-    console.log(e);
-  }
+  const [toggle, setToggle] = useState(false);
   
   return (
     <div>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="">
-              
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="">
-              
-            </IconButton>
-          }
-          title="This is the title"
-          subheader="lorem is here"
-          
-        />
-        <CardMedia style={{ width:"100%", height:"200px", objectFit:'fill'  }} component="img" title="" image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F6%2F3%2F0%2F41374.jpg&f=1&nofb=1" />
-        <CardContent>
-          <Typography sx={{fontSize:40}} color="text.secondary" gutterBottom>
-            Lorem ipsum dolor sit amet.
+      <Button variant="outlined" onClick={()=>setToggle(!toggle)} >Right</Button>
+      <Drawer
+      
+      anchor="right"
+      open={toggle}
+      onClose={()=>setToggle(!toggle)}
+      style={{ width:'500px' }}>
+        <div style={{ width:"400px", padding:"100px" }}>
+          <Typography varient="h5">
+            Home
           </Typography>
-        </CardContent>
-      </Card>
+        </div>
+      </Drawer>
       
     </div>
   );
